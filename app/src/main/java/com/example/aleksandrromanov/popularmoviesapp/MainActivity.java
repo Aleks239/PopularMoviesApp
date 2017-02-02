e9648a6e5c65b9c9c5933cdab00cfa0fb2cfd960
Aleksandr Romanov <samroproduction@gmail.com>
2017-02-02 22:13:36 +0200

    Add AsyncTask inner class and API KEY field

--- app/src/main/java/com/example/aleksandrromanov/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/aleksandrromanov/popularmoviesapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.aleksandrromanov.popularmoviesapp;
 
+
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static String LOG_TAG = MainActivity.class.getName();
+    private static String API_KEY;
 
 
 
@@ -15,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        API_KEY = getString(R.string.MOVIE_DB_API_KEY);
     }
 
     @Override
@@ -29,8 +38,11 @@ public class MainActivity extends AppCompatActivity {
         int itemId = item.getItemId();
         switch (itemId){
             case R.id.refresh_menu_item:
-                String message = "Hello";
-                Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+                URL url = NetworkUtility.buildMovieURL("popular",API_KEY);
+                if(url != null){
+                    new FetchPopularMoviesTask().execute(url);
+                }
+                else Log.d(LOG_TAG,"Error has occurred");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -38,5 +50,29 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    class FetchPopularMoviesTask extends AsyncTask<URL,Void,String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL movieURL = urls[0];
+            String movieJSON = NetworkUtility.getMovieJSON(movieURL);
+            return movieJSON;
+
+        }
+
+        @Override
+        protected void onPostExecute(String movieJsonString) {
+            if(movieJsonString != null){
+                Log.d(LOG_TAG,movieJsonString);
+            }
+
+            else{
+                Log.d(LOG_TAG,"Failed to fetch JSON");
+            }
+
+
+        }
     }
 }
