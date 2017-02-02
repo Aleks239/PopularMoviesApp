a622433adc6a38f17dcfcd209cefd8871bd8d18c
Aleksandr Romanov <samroproduction@gmail.com>
2017-02-02 18:17:35 +0200

    Add menu item click handlers

--- app/src/main/java/com/example/aleksandrromanov/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/aleksandrromanov/popularmoviesapp/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.aleksandrromanov.popularmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_resource,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId){
+            case R.id.refresh_menu_item:
+                String message = "Hello";
+                Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+
+        }
+
     }
 }
