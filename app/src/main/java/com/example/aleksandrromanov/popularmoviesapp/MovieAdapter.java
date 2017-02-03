b70c90247a3f12028773787d5447ab0bf248a511
Aleksandr Romanov <samroproduction@gmail.com>
2017-02-03 18:07:38 +0200

    Add Movie adapter and override signatures

--- app/src/main/java/com/example/aleksandrromanov/popularmoviesapp/MovieAdapter.java
+++ app/src/main/java/com/example/aleksandrromanov/popularmoviesapp/MovieAdapter.java
@@ -1,10 +1,37 @@
 package com.example.aleksandrromanov.popularmoviesapp;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by aleksandrromanov on 03/02/2017.
  */
 
-public class MovieAdapter extends RecyclerView.Adapter {
+class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MoviePosterViewHolder> {
+
+    @Override
+    public MoviePosterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MoviePosterViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MoviePosterViewHolder extends RecyclerView.ViewHolder{
+
+
+        public MoviePosterViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
 }
